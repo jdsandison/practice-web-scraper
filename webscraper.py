aca5fca4e6a98fc49c6736777825291a6e842581
JackS1832 <jacksandison11@gmail.com>
2023-07-10 15:14:44 +0100

    now scrapes the titles of each book

--- webscraper.py
+++ webscraper.py
@@ -1,6 +1,16 @@
-import pandas as pd
+from requests_html import HTMLSession
 from bs4 import BeautifulSoup
-from selenium import webdriver
 
-driver = webdriver.Chrome()
-driver.get('http://books.toscrape.com/')
+s = HTMLSession()
+url = 'https://books.toscrape.com/'
+
+def getData(url):
+    r = s.get(url)
+    soup = BeautifulSoup(r.text, 'html.parser')
+    return soup
+
+def getTitles(soup):
+    titles = soup.find_all('a', {'title': True})
+    return [title.get('title') for title in titles]
+
+print(getTitles(getData(url)))
