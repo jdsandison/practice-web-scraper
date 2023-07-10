eaab4128a2e84735da8137aaab84b64b2c25a7b7
JackS1832 <jacksandison11@gmail.com>
2023-07-10 15:27:12 +0100

    now scrapes all 1000 titles

--- webscraper.py
+++ webscraper.py
@@ -2,15 +2,23 @@ from requests_html import HTMLSession
 from bs4 import BeautifulSoup
 
 s = HTMLSession()
-url = 'https://books.toscrape.com/'
+url = 'https://books.toscrape.com/catalogue/page-1.html'
+base_url = 'https://books.toscrape.com/catalogue/page-{}.html'
+all_titles = []
 
 def getData(url):
     r = s.get(url)
     soup = BeautifulSoup(r.text, 'html.parser')
     return soup
 
+
 def getTitles(soup):
     titles = soup.find_all('a', {'title': True})
     return [title.get('title') for title in titles]
 
-print(getTitles(getData(url)))
+for page_number in range(1,51):
+    page_url = base_url.format(page_number)
+    titles = getTitles(getData(page_url))
+    all_titles.extend(titles)
+
+print(len(all_titles))
