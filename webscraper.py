e1c504fe70eccc5338a7d5dfe4f58a917317db97
JackS1832 <jacksandison11@gmail.com>
2023-07-26 20:13:36 +0100

    Update webscraper.py

--- webscraper.py
+++ webscraper.py
@@ -6,6 +6,7 @@ url = 'https://books.toscrape.com/catalogue/page-1.html'
 base_url = 'https://books.toscrape.com/catalogue/page-{}.html'
 all_titles = []
 
+
 def getData(url):
     r = s.get(url)
     soup = BeautifulSoup(r.text, 'html.parser')
@@ -16,9 +17,18 @@ def getTitles(soup):
     titles = soup.find_all('a', {'title': True})
     return [title.get('title') for title in titles]
 
-for page_number in range(1,51):
+
+for page_number in range(1, 51):
     page_url = base_url.format(page_number)
     titles = getTitles(getData(page_url))
     all_titles.extend(titles)
 
 print(len(all_titles))
+
+
+def testing():
+    for i in range(5):
+        print(len(all_titles))
+
+           
+print(testing())
