a9c38b6c5a8da10cd58a21453caac64fa4c8c2e9
Jack Sandison <jacksandison11@gmail.com>
2023-07-31 10:20:30 +0100

    finds the id of each advert on the page

--- webscraper.py
+++ webscraper.py
@@ -4,6 +4,16 @@ import csv
 
 headers = {"User-Agent" : "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36"}
 
+def find_ids(search_url):
+    response = requests.get(search_url, headers=headers)
+    soup = BeautifulSoup(response.content, "html.parser")
+    div_elements = soup.find_all("div", class_="result-item")
+    for div in div_elements:
+        adid_values = [div["adid"]]
+
+    return  adid_values
+
+
 
 def find_listings(search_url):
     response = requests.get(search_url, headers=headers)
@@ -17,7 +27,9 @@ def find_listings(search_url):
 
 def main():
     search_url = 'https://www.exchangeandmart.co.uk/used-cars-for-sale'
-    find_listings(search_url)
+    #find_listings(search_url)
+    find_ids(search_url)
+    
 
 if __name__ == "__main__":
     main()
