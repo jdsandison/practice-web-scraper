4d9fdb24d0cbd4a6e7b8754adacccce2f65e341f
Jack Sandison <jacksandison11@gmail.com>
2023-07-31 10:37:17 +0100

    created function that finds the makes and models on 1st page
    will expand the function to all the pages at a later date

--- webscraper.py
+++ webscraper.py
@@ -4,9 +4,13 @@ import csv
 
 headers = {"User-Agent" : "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36"}
 
-def find_ids(search_url):
-    response = requests.get(search_url, headers=headers)
+def get_soup(url):
+    response = requests.get(url, headers=headers)
     soup = BeautifulSoup(response.content, "html.parser")
+    return soup
+
+def find_ids(search_url):
+    soup = get_soup(search_url)
     div_elements = soup.find_all("div", class_="result-item")
     for div in div_elements:
         adid_values = [div["adid"]]
@@ -14,22 +18,18 @@ def find_ids(search_url):
     return  adid_values
 
 
-
-def find_listings(search_url):
-    response = requests.get(search_url, headers=headers)
-    soup = BeautifulSoup(response.content, "html.parser")
-
-    parent_div = soup.find("div", adid="30186438")
-    car_titles = parent_div.find("div", class_="result-item__description hidden-xs")
-    for title in car_titles:
-        car_title = title.text.strip()
-        print(car_title)
+def  find_makes_and_models(search_url):
+    soup = get_soup(search_url)
+    div_elements = soup.find_all("div", class_="result-item")
+    for div in div_elements:
+        makes = [div["make"]]
+        models = [div["model"]]
+        print(makes, models)
 
 def main():
     search_url = 'https://www.exchangeandmart.co.uk/used-cars-for-sale'
-    #find_listings(search_url)
-    find_ids(search_url)
-    
+    #find_ids(search_url)
+    find_makes_and_models(search_url)
 
 if __name__ == "__main__":
     main()
