b754531cc60c877797a2902a1d5db69fe654718b
Jack Sandison <jacksandison11@gmail.com>
2023-07-31 14:27:07 +0100

    Update webscraper.py

--- webscraper.py
+++ webscraper.py
@@ -1,6 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
 import csv
+import json
 
 headers = {"User-Agent" : "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36"}
 
@@ -9,27 +10,20 @@ def get_soup(url):
     soup = BeautifulSoup(response.content, "html.parser")
     return soup
 
-def find_ids(search_url):
-    soup = get_soup(search_url)
-    div_elements = soup.find_all("div", class_="result-item")
-    for div in div_elements:
-        adid_values = [div["adid"]]
 
-    return  adid_values
-
-
-def  find_makes_and_models(search_url):
+def get_info(search_url):
     soup = get_soup(search_url)
     div_elements = soup.find_all("div", class_="result-item")
     for div in div_elements:
         makes = [div["make"]]
         models = [div["model"]]
-        print(makes, models)
+        id_values = [div["adid"]]
+        print(makes, models, id_values)
 
 def main():
-    search_url = 'https://www.exchangeandmart.co.uk/used-cars-for-sale'
-    #find_ids(search_url)
-    find_makes_and_models(search_url)
+    search_url = 'https://www.exchangeandmart.co.uk/used-cars-for-sale/any-distance-from-dn3-3eh'
+    get_info(search_url)
+
 
 if __name__ == "__main__":
     main()
