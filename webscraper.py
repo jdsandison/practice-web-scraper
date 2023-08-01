3f576bfbdfb186dba06770849c689bcd80ded206
Jack Sandison <jacksandison11@gmail.com>
2023-08-01 16:03:19 +0100

    added some dataframe manipulation
    still doesnt work fully as intended. But using pandas has made the data storage easier to work with in future

--- webscraper.py
+++ webscraper.py
@@ -9,11 +9,6 @@ makes = []
 models = []
 id_values = []
 
-# creating an empty dataframe
-columns = ["Make", "Model", "Ad_ID", "Wheel drive", "Doors", "Seats", "Engine power", "Top speed", "Acceleration (0-62 mph)", "CO2 rating", "Annual tax"]
-df = pd.DataFrame(columns=columns)
-
-
 
 def get_soup(url):
     response = requests.get(url, headers=headers)
@@ -22,6 +17,8 @@ def get_soup(url):
 
 
 def get_info(search_url):
+    global makes, models, ad_id
+    
     soup = get_soup(search_url)
     div_elements = soup.find_all("div", class_="result-item")
 
@@ -35,16 +32,18 @@ def get_info(search_url):
             models.append(model)
             id_values.append(ad_id)
 
-    print(id_values, makes, models)
+    #print(id_values, makes, models)
 
 
 def manufacture_link(advert_base_url, id):
     return advert_base_url + "/ad/" + str(id)
 
 
-def advert_info(url):
+def advert_info(url, makes, models, id_values):
     get_info(url)
     specs_list = []
+    columns = ["Make", "Model", "Ad_ID", "Wheel drive", "Doors", "Seats", "Engine power", "Top speed", "Acceleration (0-62 mph)", "CO2 rating", "Annual tax"]
+    df = pd.DataFrame(columns=columns)
     for i in range(0, len(id_values)):
         current_advert_link = manufacture_link("https://www.exchangeandmart.co.uk", id_values[i])
         soup = get_soup(current_advert_link)
@@ -55,23 +54,21 @@ def advert_info(url):
             key = data[0].strip(':')
             value = data[1]
             specs[key] = value
-        specs_list.append(specs)
+            specs_list.append(specs)
+            
+        df_updated = df.copy()    
+        for i in range(len(id_values)):
+            values_to_write = [specs.get(info, '') for info in columns[3:]]
+            df_row = dict(zip(columns[3:], values_to_write))
+            df_row['Make'] = makes[i]
+            df_row['Model'] = models[i]
+            df_row['Ad_ID'] = id_values[i]
+            df_updated = pd.concat([df_updated, pd.DataFrame([df_row])], ignore_index=True)    
 
-    with open("specs.csv", "w", newline="") as csvfile:
-        info_included = ["Wheel drive", "Doors", "Seats", "Engine power", "Top speed", "Acceleration (0-62 mph)", "CO2 rating", "Annual tax"]
-        writer = csv.writer(csvfile)
-        writer.writerow(info_included)
-        for specs in specs_list:
-            values_to_write = []
-
-            for info in info_included:
-                value = specs.get(info, '')
-                if info == 'Annual tax':
-                    value = value.replace('£', '')
-                values_to_write.append(value)
-
-            writer.writerow(values_to_write)
-
+        if 'Annual tax' in specs:
+            df['Annual tax'] = df['Annual tax'].str.replace('£', '')
+    
+        print(df_updated)
 
 def main():
     base_url = 'https://www.exchangeandmart.co.uk/used-cars-for-sale/under-1-miles-from-dn3-3eh/page'
@@ -84,7 +81,7 @@ def main():
         if not soup.find("div", class_="result-item"):
             break
 
-        advert_info(current_url)
+        advert_info(current_url, makes, models, id_values)
         page_number += 1
 
 
