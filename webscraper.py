e1cdd3a94f43ebf50ef89352b8f4e8a613acc1d6
Jack Sandison <jacksandison11@gmail.com>
2023-08-16 20:15:50 +0100

    started scraping each model one at a time
    theres an issue with storing the data, often its repeating itself

--- webscraper.py
+++ webscraper.py
@@ -9,6 +9,10 @@ makes = []
 models = []
 id_values = []
 available_makes_filtered = []
+ids_of_makes = []
+makes2 = []
+models2 = []
+id_values2 = []
 
 
 def get_soup(url):
@@ -17,13 +21,39 @@ def get_soup(url):
     return soup
 
 
+def get_car_models(base_url):
+    global available_makes_filtered
+    soup = get_soup(base_url)
+    available_makes_unfiltered = []
+
+    list_of_available_makes = soup.find_all("div", class_="c-column-section")
+    list_of_available_makes = list_of_available_makes[1]
+    for potential_make in list_of_available_makes:
+        make_text = potential_make.text
+        available_makes_unfiltered.append(make_text)
+
+    for i in range(len(available_makes_unfiltered)):
+        if available_makes_unfiltered[i] == '\n':
+            pass
+        else:
+            available_makes_filtered.append(available_makes_unfiltered[i])
+
+    available_makes_filtered = [brand.replace('Used ', '') for brand in available_makes_filtered]
+
+    for idx, make in enumerate(available_makes_filtered):
+        if make in ['Alfa Romeo', 'Aston Martin', 'Land Rover']:
+            available_makes_filtered[idx] = make.replace(' ','-')
+
+    return available_makes_filtered
+
+
 def get_page_info(search_url):
     """
     :param search_url: the url of the listings on a specific page number
     :return: search_dataframe: dataframe containing all the makes, models and id of specific ad pages
     """
 
-    global makes, models, id_values, available_makes_filtered
+    global makes, models, id_values
     soup = get_soup(search_url)
     div_elements = soup.find_all("div", class_="result-item")
 
@@ -44,22 +74,6 @@ def get_page_info(search_url):
     }
 
     search_dataframe = pd.DataFrame(search_data)
-    available_makes_unfiltered = []
-
-    list_of_available_makes = soup.find_all("div", class_="c-column-section")
-    list_of_available_makes = list_of_available_makes[1]
-    for potential_make in list_of_available_makes:
-        make_text = potential_make.text
-        available_makes_unfiltered.append(make_text)
-
-    for i in range(len(available_makes_unfiltered)):
-        if available_makes_unfiltered[i] == '\n':
-            pass
-        else:
-            available_makes_filtered.append(available_makes_unfiltered[i])
-
-    available_makes_filtered = [brand.replace('Used ', '') for brand in available_makes_filtered]
-    print(available_makes_filtered)
 
     return search_dataframe
 
@@ -83,6 +97,33 @@ def removing_nan_values(dataframe):
     dataframe.reset_index(drop=True, inplace=True)
 
     return dataframe
+
+
+def get_ids_of_each_make(make, page_number):
+    global ids_of_makes, makes2, models2, id_values2
+    make_url = 'https://www.exchangeandmart.co.uk/used-cars-for-sale/any-distance-from-b30-3aa/' + str(make) + '/page' + str(page_number)
+    soup = get_soup(make_url)
+    elements = soup.find_all("div", class_="result-item")
+    for div in elements:
+        make = (div.get("make", "Unknown make"))
+        model = (div.get("model", "Unknown model"))
+        ad_id = (div.get("adid", "Unknown id"))
+
+        if make != "Unknown make" and model != "Unknown model" and ad_id != "unknown id":
+            makes2.append(make)
+            models2.append(model)
+            id_values2.append(ad_id)
+
+    search_data = {
+        'Makes': makes2,
+        'Models': models2,
+        'ID values': id_values2
+    }
+
+    #search_dataframe = pd.DataFrame(search_data)
+
+    #return search_dataframe
+    return search_data['ID values']
 
 
 def advert_info():
@@ -167,7 +208,7 @@ def advert_info():
 def main():
     base_url = 'https://www.exchangeandmart.co.uk/used-cars-for-sale/any-distance-from-b30-3aa/page'
     page_number = 1
-    max_page = 5
+    max_page = 3
 
     while True:
         current_url = base_url + str(page_number)
@@ -181,21 +222,31 @@ def main():
 
         first_dataframe = get_page_info(current_url)
 
+        get_car_models(base_url)
+        for make in available_makes_filtered:
+            output_ids = get_ids_of_each_make(make, page_number)
+            print(output_ids)
+            ids_of_makes.append(output_ids)
+            print('working!' + make)
+
         page_number += 1
 
-    second_dataframe = advert_info()
+    print(ids_of_makes)
+    print('didnt work')
 
-    final_dataframe = pd.concat([first_dataframe, second_dataframe], axis=1)
-
-    rows_with_nan_values = []
-    for index, row in final_dataframe.iterrows():
-        if row.isna().any():
-            rows_with_nan_values.append(index)
-
-    final_dataframe.drop(rows_with_nan_values, inplace=True)
-    final_dataframe.reset_index(drop=True, inplace=True)
-
-    final_dataframe.to_csv('data.csv', index=False, encoding='utf-8')
+    # second_dataframe = advert_info()
+    #
+    # final_dataframe = pd.concat([first_dataframe, second_dataframe], axis=1)
+    #
+    # rows_with_nan_values = []
+    # for index, row in final_dataframe.iterrows():
+    #     if row.isna().any():
+    #         rows_with_nan_values.append(index)
+    #
+    # final_dataframe.drop(rows_with_nan_values, inplace=True)
+    # final_dataframe.reset_index(drop=True, inplace=True)
+    #
+    # final_dataframe.to_csv('data.csv', index=False, encoding='utf-8')
 
 if __name__ == "__main__":
     main()
