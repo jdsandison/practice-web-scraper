dc3519ba6bca1b1ab0df044349ce96adf7e4b94e
Jack Sandison <jacksandison11@gmail.com>
2023-08-20 14:00:10 +0100

    scraping information but not outputting to data file

--- webscraper.py
+++ webscraper.py
@@ -1,5 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
+import numpy as np
 import pandas as pd
 
 headers = {"User-Agent" : "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36"}
@@ -49,15 +50,20 @@ def create_url(advert_base_url, id):
 
 def advert_info(url, current_id):
 
+    first_data = {'makes and models': None,
+                  'Id value': current_id}
+
     full_table_data = []
     accepted_ids = []
     rejected_ids_for_spec = []
 
     specs_list = []
     accumulated_data = []
-    types_of_data = ['Accepted id value', 'Year', 'Engine size', 'Mileage', 'Fuel type', 'Transmission', 'Colour', 'Body type', 'Mpg']
+    types_of_data = ['Year', 'Engine size', 'Mileage', 'Fuel type', 'Transmission', 'Colour', 'Body type', 'Mpg']
     soup = get_soup(url)
     specs = {}
+    make_and_model = soup.find('h2', class_='col-xs-9').find('span', class_='ttl')
+    first_data['makes and models'] = make_and_model.get_text(strip=True)
     specification_tab = soup.find_all("div", class_="adSpecItem")
     table_of_info = soup.find_all("div", class_="adDetsItem")
     for item in specification_tab:
@@ -83,64 +89,44 @@ def advert_info(url, current_id):
         full_table_data.append(accumulated_data[-1])
     else:
         print('not all data present', list_of_table_data, 'rejected id value', current_id)
+        return
 
     specification_tab_dataframe = pd.DataFrame(specs_list)
     table_data_dataframe = pd.DataFrame(full_table_data)
-    accepted_ids_dataframe = pd.DataFrame(accepted_ids)
-    table_data_and_id_dataframe = pd.concat([accepted_ids_dataframe, table_data_dataframe], axis=1)
-    table_data_and_id_dataframe.columns = types_of_data
-    specification_tab_and_id = pd.concat([accepted_ids_dataframe, specification_tab_dataframe], axis=1)
+    table_data_dataframe.columns = types_of_data
     wanted_columns = ['Makes', 'Models', 'ID values', 'Year', 'Engine size', 'Mileage',
                   'Fuel type', 'Transmission', 'Colour', 'Body type', 'Mpg',
                   'Wheel drive', 'Doors', 'Seats', 'Engine power', 'Top speed',
                   'acceleration', 'CO2 rating', 'Tank range']
-    specification_tab_and_id = specification_tab_and_id.filter[wanted_columns]
+    specification_tab_dataframe = specification_tab_dataframe.filter(items=wanted_columns)
+    make_and_model_and_id_df = pd.DataFrame(first_data, index=[0])
 
-    for advert_id in rejected_ids_for_spec:
-        rows_to_drop_table = table_data_and_id_dataframe[table_data_and_id_dataframe.iloc[:, 0] == advert_id].index
-        rows_to_drop_specification = specification_tab_and_id[specification_tab_and_id.iloc[:, 0] == advert_id].index
-        table_data_and_id_dataframe.drop(rows_to_drop_table, inplace=True)
-        table_data_and_id_dataframe.reset_index(drop=True, inplace=True)
-        specification_tab_and_id.drop(rows_to_drop_specification, inplace=True)
-        specification_tab_and_id.reset_index(drop=True, inplace=True)
-        print('Dropping rows with', advert_id)
-
-    rows_with_nan_values = []
-    for index, row in specification_tab_and_id.iterrows():
+    for index, row in specification_tab_dataframe.iterrows():
         if row.isna().any():
-            rows_with_nan_values.append(index)
+            print('full spec data is not present')
+            return
 
-    print(rows_with_nan_values)
-    specification_tab_and_id.drop(rows_with_nan_values, inplace=True)
-    specification_tab_and_id.reset_index(drop=True, inplace=True)
-    table_data_and_id_dataframe.drop(rows_with_nan_values, inplace=True)
-    table_data_and_id_dataframe.reset_index(drop=True, inplace=True)
-
-    table_data_and_id_dataframe = table_data_and_id_dataframe.drop(columns=table_data_and_id_dataframe.columns[0])
-    specification_tab_and_id = specification_tab_and_id.drop(columns=specification_tab_and_id.columns[0])
-
-    combined_dataframe = pd.concat([table_data_and_id_dataframe, specification_tab_and_id], axis=1)
-
+    combined_dataframe = pd.concat([make_and_model_and_id_df, table_data_dataframe, specification_tab_dataframe], ignore_index=True)
     return combined_dataframe
 
 
 def main():
     still_searching = True
-    starting_number = 29000000
-    max_consecutive_inactive_ids = 1000
+    max_consecutive_inactive_ids = 50
     current_consecutive_inactive_ids = 0
     base_url = 'https://www.exchangeandmart.co.uk/ad/'
     data_file_column_length = len(data_file['ID values']) - 1
-    current_id_number = data_file.iat[data_file_column_length, data_file.get_loc('ID values')]
+    current_id_number = data_file.iat[data_file_column_length, data_file.columns.get_loc('ID values')]
 
     while still_searching:
         if current_consecutive_inactive_ids > max_consecutive_inactive_ids:
+            updated_data.to_csv('data.csv', index=False, encoding='utf-8')
             still_searching = False
         else:
             soup = get_soup(base_url + str(current_id_number))
             if soup.find("div", id="vehicle-desc"):
                 current_consecutive_inactive_ids = 0
-                data_file.append(advert_info(base_url + str(current_id_number), current_id_number))
+                updated_data = pd.concat([data_file, (advert_info(base_url + str(current_id_number), current_id_number))], ignore_index=True)
                 current_id_number += 1
             else:
                 current_consecutive_inactive_ids += 1
