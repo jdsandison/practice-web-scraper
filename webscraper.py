c935075da593d66d0f928e10ddd65e03d3f00d5b
Jack Sandison <jacksandison11@gmail.com>
2023-08-20 20:49:46 +0100

    Update webscraper.py

--- webscraper.py
+++ webscraper.py
@@ -6,9 +6,6 @@ headers = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 "
                   "Safari/537.36"}
 
-makes = []
-models = []
-id_values = []
 
 data_file = pd.read_csv(r'data.csv')
 
