3a0bc24f794f4350669d00dc2bc320a9271fe245
Jack Sandison <jacksandison11@gmail.com>
2023-09-16 16:10:09 +0100

    created a table for body_types and inserted the ids into the dataset
    Similarly to the fuel_types I added the body_type id's to the dataset. Building a relational database

--- database.py
+++ database.py
@@ -23,12 +23,14 @@ def main():
         print(x)
     fuel_name = ""
     fuel_type_id = 0
+    body_type_id = 0
     row = 0
-    column = 5
+    column_fuel = 5
+    column_body_types = 8
 
     number_of_rows = len(file['ID value'])
     for row in range(number_of_rows):
-        fuel_name = file.iat[row, column]
+        fuel_name = file.iat[row, column_fuel]
         mycursor.execute("SELECT * FROM fuel_types WHERE fuel_type = '" + fuel_name + "' LIMIT 1")
         record = mycursor.fetchone()
 
@@ -42,16 +44,35 @@ def main():
 
         else:
             # fuel type exists get the id
-            print(record[0])
-            # for x in record:
-            #    print(x)
             fuel_type_id = record[0]
-            #    print('bob' + fuel_type_id)
 
 
         # at this point, we have fuel_type_id, so insert a row into DATA with this fuel type ID.
-        statement = "INSERT INTO `webscraper`.`dataset` (`Make and model`, fuel_type_id) VALUES ('" + file.iat[row, 0] +"'," + str(fuel_type_id) +")"
-        mycursor.execute(statement)
+        #statement = "INSERT INTO `webscraper`.`dataset` (`Make and model`, fuel_type_id) VALUES ('" + file.iat[row, 0] +"'," + str(fuel_type_id) +")"
+        #mycursor.execute(statement)
+
+        body_type_name = file.iat[row, column_body_types]
+        mycursor.execute("SELECT * FROM body_types WHERE body_type = '" + body_type_name + "' LIMIT 1")
+        record2 = mycursor.fetchone()
+
+        # if LIMIT is 1 then mycursor.rowcount is always 1? Therefore, we should see when record2 is not present?
+        if record2 is None:
+            statement = "INSERT INTO `webscraper`.`body_types` (`body_type`) VALUES ('" + body_type_name + "');"
+            mycursor.execute(statement)
+            body_type_id = mycursor.lastrowid
+
+        else:
+            body_type_id = record2[0]
+
+
+        print('body type id: ' + str(body_type_id))
+
+        statement2 = "INSERT INTO `webscraper`.`dataset` (`Make and model`, fuel_type_id, body_type_id) VALUES ('" + file.iat[row, 0] + "'," + str(fuel_type_id) +", " + str(body_type_id) + ")"
+        mycursor.execute(statement2)
+    # similarly with the body type column, include a table of id's with respective body tpes (coupe, hatchback ect)
+    # then split the make and model column into makes, models. And then create a table of each that number them with respective ids.
+
+
 if __name__ == "__main__":
     main()
 
